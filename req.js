9b42097445838e4b86b1aa874edbce4de903a8be
Kostadin Golev <kotseto@gmail.com>
2014-01-24 17:06:09 +0200

    some more debug added

--- req.js
+++ req.js
@@ -1,4 +1,5 @@
 var https = require('https');
+var http = require('http');
 
 var body = '<?xml version="1.0" encoding="utf-8"?>' +
            '<someRequest></someRequest>';
@@ -6,7 +7,7 @@ var body = '<?xml version="1.0" encoding="utf-8"?>' +
 // var host = "http://shrouded-woodland-1421.herokuapp.com"
 // var port = 443;
 var host = "localhost";
-var port = 8443;
+var port = 8000;
 
 var postRequest = {
     host: host,
@@ -16,13 +17,15 @@ var postRequest = {
     headers: {
         'Content-Type': 'text/xml',
         'Content-Length': Buffer.byteLength(body)
-    },
-    rejectUnauthorized: false
+    }
+    // ,
+    // rejectUnauthorized: false
 };
 
 var buffer = "";
 
-var req = https.request( postRequest, function( res )    {
+// var req = https.request( postRequest, function( res )    {
+var req = http.request( postRequest, function( res )    {
 
    console.log( res.statusCode );
    var buffer = "";
--- web.js
+++ web.js
@@ -1,14 +1,9 @@
 // Create a HTTP server on port 8000
 // Send plain text headers and 'Hello World' to each client
  
-var http = require('http');
-
-
 var port = process.env.PORT || 8000;
- 
 var http = require('http');
 var counter = 0;
-
 
 var server = http.createServer(function (req, res) {
   // req is an http.IncomingMessage, which is a Readable Stream
@@ -26,13 +21,17 @@ var server = http.createServer(function (req, res) {
 
   // the end event tells you that you have entire body
   req.on('end', function () {
+    console.log('received ' + req.method + ' request from: ' + req.connection.remoteAddress);
 
-	counter++;
-	console.log(body);
+  	counter++;
+  	console.log('Message Start: \n' + body);
+    console.log('Message End: \n');
 
-	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
-    res.end("requestor # " + counter + ": request was : " + body);
-  })
+  	res.writeHead(200, {'Content-Type': 'text/html'}); // prepare response headers
+      res.end("requestor # " + counter + ": request was : " + body);
+    })
+
+  // console.log(req);
 })
 
 console.log('Server running at http://127.0.0.1:' + port);
